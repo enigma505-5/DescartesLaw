d05e2452f625b274b1fa2f2a5c14584f3aac148e
enigma505-5 <youknowtheacciswell29@outlook.com>
2025-11-30 13:03:29 +0100

    Remove unused fetch function from main.js

--- src/main.js
+++
@@ -1,9 +0,0 @@
-fetch('/.netlify/functions/tr', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-        page: window.location.pathname
-    })
-});
